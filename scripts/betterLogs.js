947f56409404be3099564eef7500ec476bcfe5c8
Jarrod McEvers <jarrodamcevers@gmail.com>
2019-01-24 20:54:26 -0600

    Use reg exp so it only works on the service page

--- scripts/betterLogs.js
+++ scripts/betterLogs.js
@@ -1,14 +1,13 @@
 window.onhashchange = function () {
-  let ul = document.getElementsByClassName('aws-tab-bar');
+  let ul        = document.getElementsByClassName('aws-tab-bar');
   let li        = document.createElement('li');
   let a         = document.createElement('a');
   a.textContent = 'Better Logs';
 
-  let link = window.location.href;
-  if (link.indexOf('console.aws.amazon.com/ecs/') >= 0) {
-    let splitLink = link.split('/');
-    let appName   = splitLink[splitLink.length - 2];
-
+  let link  = window.location.href;
+  let match = link.match(new RegExp('clusters/.*/services/(.*)/'));
+  if (match) {
+    let appName = match[1];
     a.setAttribute('href', `https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logs:prefix=${appName}`);
     a.setAttribute('target', '_blank');
     li.appendChild(a);
