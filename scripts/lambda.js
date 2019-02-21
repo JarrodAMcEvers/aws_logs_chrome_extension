9107c40ced0229604c9002fc7767d00652674b5c
Jarrod McEvers <jarrodamcevers@gmail.com>
2019-02-20 21:46:00 -0600

    Put better logs button on lambda function dashboard

--- scripts/lambda.js
+++ scripts/lambda.js
@@ -1,23 +1,27 @@
 const betterLogs = () => {
   setTimeout(() => {
-    let table = document.getElementsByTagName('table')[0];
+    let div = document.getElementsByClassName('awsui-util-f-r awsui-util-mt-s');
+    div     = div[0];
 
-    for (let i = 0, row; row = table.rows[i]; i++) {
-      let row = table.rows[i];
-      if (row.getElementsByTagName('th').length === 0) {
-        let link         = document.createElement('a');
-        link.textContent = 'Logs';
-        link.setAttribute('href', `https://console.aws.amazon.com/cloudwatch/home?region=us-east-1#logStream:group=/aws/lambda/${row.cells[1].innerText}`);
+    let button = document.createElement('awsui-button');
+    button.classList.add('awsui-button');
+    button.classList.add('awsui-button-variant-normal');
+    button.classList.add('awsui-hover-child-icons');
+    button.setAttribute('type', 'submit');
 
-        let newTD = row.insertCell(2);
-        newTD.append(link);
-      } else {
-        let newTH       = document.createElement('th');
-        newTH.innerText = 'Better Logs';
-        row.insertBefore(newTH, row.getElementsByTagName('th')[2]);
-      }
-    }
-  }, 2500);
+    let hash         = window.location.hash;
+    let functionName = hash.substring(0, hash.indexOf('?')).split('functions/')[1];
+    let url          = `${window.location.origin}/cloudwatch/home?#logs:prefix=/aws/lambda/${functionName}`;
+
+    let span         = document.createElement('span');
+    span.textContent = 'Better Logs';
+    span.setAttribute('awsui-button-region', 'text');
+    span.setAttribute('onclick', `window.open('${url}', "_blank")`);
+    span.setAttribute('target', '_blank');
+
+    button.append(span);
+    div.append('\t', button);
+  }, 3500);
 };
 
 window.onhashchange = betterLogs;
